3f52ca1df8c3bfe2394ab96167be27206499de0f
shadow578 <52449218+shadow578@users.noreply.github.com>
2021-10-27 19:09:23 +0200

    implement Developer Flags

--- app/src/main/kotlin/io/github/shadow578/yodel/ui/dev/DeveloperToolsActivity.kt
+++ app/src/main/kotlin/io/github/shadow578/yodel/ui/dev/DeveloperToolsActivity.kt
@@ -1,7 +1,10 @@
 package io.github.shadow578.yodel.ui.dev
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.switchmaterial.SwitchMaterial
 import io.github.shadow578.yodel.databinding.ActivityDeveloperToolsBinding
 import io.github.shadow578.yodel.ui.base.BaseActivity
 import io.github.shadow578.yodel.util.copyToClipboard
@@ -47,10 +50,25 @@ class DeveloperToolsActivity : BaseActivity() {
             model.reloadAllTracks()
         }
 
-        // bind switches with preferences
-        model.enableDownloaderErrorNotificationsBinder.bind(this, b.downloaderErrorNotifications)
-        model.enableDownloaderVerboseOutputBinder.bind(this, b.downloaderVerboseOutput)
-        model.enableSSLFixBinder.bind(this, b.enableSslFix)
-        model.useVideoIdOnlyBinder.bind(this, b.downloaderUseVideoId)
+        // init and bind switches for dev flags
+        model.bindAllFlags { flag, binder ->
+            // create the material switch
+            val switch = SwitchMaterial(this).apply {
+                layoutParams = ViewGroup.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.WRAP_CONTENT
+                )
+
+                // suppressing this warning since this is in devtools and we don't care about hardcoded strings here
+                @SuppressLint("SetTextI18n")
+                text = "${flag.displayName} \r\n#${flag.key}"
+            }
+
+            // bind switch to preference
+            binder.bind(this, switch)
+
+            // add the switch to the container
+            b.flagsContainer.addView(switch)
+        }
     }
 }
