dcb22776f885e03ebf4d67b4f1265074e93ca337
shadow578 <52449218+shadow578@users.noreply.github.com>
2024-11-11 19:49:56 +0100

    fix setup-java action

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         minSdk = 24
         targetSdk = 33
         compileSdk = 33
-        versionCode = 7
-        versionName = "1.4.2"
+        versionCode = 8
+        versionName = "1.4.3"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         kapt {
